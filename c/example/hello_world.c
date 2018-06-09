bb6bf1f1df1804e8d47b178c82744fabff0a1fad
alpha-gj <Guang-Jie_Jhang@alphanetworks.com>
2018-06-09 10:24:49 +0800

    Add error message with exit in example

--- c/example/hello_world.c
+++ c/example/hello_world.c
@@ -1,5 +1,11 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define err_exit(MESSAGE) ( \
+	perror(MESSAGE),		\
+	exit(1)					\
+)
 
 int main(void)
 {
