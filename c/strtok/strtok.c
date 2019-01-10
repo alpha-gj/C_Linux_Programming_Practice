32c54ea4a0bb78432aa8ca04a25d10cb7b1d0a1a
alpha-gj <Guang-Jie_Jhang@alphanetworks.com>
2019-01-10 11:59:55 +0800

    [strtok] add output to array for splited chunks

--- c/strtok/strtok.c
+++ c/strtok/strtok.c
@@ -8,13 +8,23 @@ int main()
   char *delim = ":";
   char * pch;
 
+  char *array[6];
+  int i = 0;
+
   printf ("Splitting string \"%s\" into tokens:\n",str);
 
   pch = strtok(str,delim);
   while (pch != NULL) {
     printf ("%s\n",pch);
+	array[i++] = pch;
     pch = strtok (NULL, delim);
   }      
+
+  printf("Output ot Array\n");
+
+  for (i = 0; i < 6; i++) {
+	  printf("%s\n", array[i]);
+  }
 
   return 0;
 }
