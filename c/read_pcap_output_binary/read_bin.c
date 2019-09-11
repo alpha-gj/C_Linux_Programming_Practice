d93cbdc39e816f4ab03a9faf70e90bc09cb16ca8
jhanggj <m10217009@gmail.com>
2019-09-11 10:39:45 +0800

    Delete Blank

--- c/read_pcap_output_binary/read_bin.c
+++ c/read_pcap_output_binary/read_bin.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #define ONE_BYTE 1
@@ -12,10 +12,10 @@
 #define IP_HEADER_LEN	20
 #define TCP_HEADER_LEN	32
 
-// OFFSET base on tail offset of pattern_serverIP_to_clientIP 
+// OFFSET base on tail offset of pattern_serverIP_to_clientIP
 #define OFFSET_HEAD_TCP_FLAG			13	// ACK, PSH, ....
 #define OFFSET_HEAD_IP_PACKET_LEN		18	// IP PACKET LEN is IP_HEADER_LEN + IP_PAYLOAD_LEN
-#define OFFSET_HEAD_IP_PAYLOAD			32	 
+#define OFFSET_HEAD_IP_PAYLOAD			32
 #define OFFSET_HEAD_IP_FLAG				13
 unsigned char pattern_serverIP_to_clientIP[] = {0xAC, 0x27, 0x8D, 0x0B, 0xAC, 0x27, 0x8D, 0xA3};  //ip.src == 172.39.141.11 && ip.dst == 172.39.141.163
 
@@ -34,28 +34,28 @@ bool hasPSH_ACK = false;	// no more payload
 
 int main(int argc, char* argv[]) {
 
-    if(argc != 2) { 
-        puts("指令: read <filename>"); 
-        eturn 1; 
-    } 
+    if(argc != 2) {
+        puts("指令: read <filename>");
+        return 1;
+    }
 
     FILE *file = fopen(argv[1], "rb");
-    if(!file) { 
-        puts("無法讀取檔案"); 
-        return 1; 
-    } 
+    if(!file) {
+        puts("無法讀取檔案");
+        return 1;
+    }
 
-    unsigned char ch; 
-    int count = 0; 
+    unsigned char ch;
+    int count = 0;
 	int checking_count = 0;
 	char buffer_checking[8] = {};
 	int pattern_serverIP_to_clientIP_count = 0;
 
-	const int checking_count_ok = sizeof(pattern_serverIP_to_clientIP)/	
+	const int checking_count_ok = sizeof(pattern_serverIP_to_clientIP)/
 								  sizeof(pattern_serverIP_to_clientIP[0]);
 	memset(buffer_checking, 0x00, checking_count_ok);
 
-    while(!feof(file)) { 
+    while(!feof(file)) {
 
 		// read every byte and put it in buffer
         fread(&ch, sizeof(char), ONE_BYTE, file);
@@ -63,7 +63,7 @@ int main(int argc, char* argv[]) {
 		memcpy(&buffer_checking[checking_count], &ch, ONE_BYTE);
 
 		// do memcmp every byte in buffer
-		if (memcmp(&pattern_serverIP_to_clientIP[checking_count], &buffer_checking[checking_count], ONE_BYTE) == 0) { 
+		if (memcmp(&pattern_serverIP_to_clientIP[checking_count], &buffer_checking[checking_count], ONE_BYTE) == 0) {
 
 			checking_count++;
 
@@ -96,8 +96,8 @@ int main(int argc, char* argv[]) {
 				fseek(file, OFFSET_HEAD_IP_FLAG, SEEK_CUR);
 				fread(&ch, sizeof(char), ONE_BYTE, file);
 				printf("TCP FLAG is %2x \n", ch);
-				
-				//STEP2.1 check the tcp flag 
+
+				//STEP2.1 check the tcp flag
 				if (ch == 0x18) {// PSH & ACK
 					hasPSH_ACK = true;
 				} else if (ch == 0x10) {
@@ -106,7 +106,7 @@ int main(int argc, char* argv[]) {
 
 				//STEP2.2 decide to memcpy payload or not
 				if (hasACK && hasPSH_ACK || hasACK) {
-					
+
 				} else {
 					hasPSH_ACK = false;
 					hasACK = false;
@@ -120,10 +120,10 @@ int main(int argc, char* argv[]) {
 
 //				if (object_packet.payload_postion != 0) // start from next index when payload postion is not 0
 //					tcp_payload_position_base++;
-					
+
 				//STEP3. fseek to the HEAD position of payload of tcp packet
 				fseek(file, OFFSET_HEAD_IP_PAYLOAD, SEEK_CUR);
-				
+
 				printf("\n======= [start] payload memcpy  & printf =======\n");
 				for (int i = 0; i < tcp_payload_len; i++) {
 
@@ -131,16 +131,16 @@ int main(int argc, char* argv[]) {
 					memcpy(&object_packet.tcp_payload_content[i + tcp_payload_position_base], &ch, ONE_BYTE);
 					printf("%2X ", object_packet.tcp_payload_content[i + tcp_payload_position_base]);
 
-					if ( i > 0 && i%16 == 0) {  // 換行 
+					if ( i > 0 && i%16 == 0) {  // 換行
 						putchar('\n');
-					} 
+					}
 					object_packet.payload_postion++;
 				}
 
 				printf("======= [end] payload memcpy  & printf =======\n");
-				
+
 //
-				//TODO STEP4. 
+				//TODO STEP4.
 
 				FILE *fp = NULL;
 				fp = fopen("./tcp_payload.bin", "w+");
@@ -154,15 +154,15 @@ int main(int argc, char* argv[]) {
 						printf("%2X ", object_packet.tcp_payload_content[i]);
 						fwrite(&object_packet.tcp_payload_content[i], sizeof(unsigned char), 1, fp);
 
-//						if ( i > 0 && i%16 == 0) {  // 換行 
+//						if ( i > 0 && i%16 == 0) {  // 換行
 //							printf("\n");
-//						} 
+//						}
 					}
 					printf("======= [end] decode payload==\n");
 				fclose(fp);
 
 					return 0;
-					//reset 
+					//reset
 					object_packet.payload_index = 0;
 					memset(object_packet.tcp_payload_content, 0x00, OBJECT_PAYLOAD_SIZE);
 				}
@@ -178,8 +178,8 @@ int main(int argc, char* argv[]) {
 			/* prevent lose some vaild data
 			example: you want to find hex string "ABCD" as factor
 			Input is "AABCD...", if you use the algorithms above mentioned,
-			you will get lose this one when your checking count is  > 0 
-			So, we need to back one step when you checking count is > 0. 
+			you will get lose this one when your checking count is  > 0
+			So, we need to back one step when you checking count is > 0.
 			Then, you can find the one time that you want */
 			fseek(file, -1, SEEK_CUR);
 			checking_count = 0;
@@ -192,16 +192,16 @@ int main(int argc, char* argv[]) {
 
 
 		// printf
-		count++; 
-		if(count > 15) {  // 換行 
+		count++;
+		if(count > 15) {  // 換行
 			putchar('\n');
-			count = 0; 
-		} 
-    } 
+			count = 0;
+		}
+    }
     putchar('\n');
 
     fclose(file);
 
 	printf("pattern_serverIP_to_clientIP_count is %d\n", pattern_serverIP_to_clientIP_count);
-    return 0; 
-} 
+    return 0;
+}
